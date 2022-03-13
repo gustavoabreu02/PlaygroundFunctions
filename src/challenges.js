f75c95ffe652fb496faa292996c441fbae84e548
Gustavo Abreu <gustavofart371@gmail.com>
2022-03-13 12:24:54 -0300

    requisito 5.1

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,10 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  wins = wins * 3
+  ties = ties * 1
+  return wins + ties
 }
 
 // Desafio 6
