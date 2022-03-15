e1af019446d4d296297aca4c54b7d6c11a1aabcd
Gustavo Abreu <gustavofart371@gmail.com>
2022-03-15 12:18:48 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,51 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numero) {
+  let resultado = '';
+  let repete = 0;
+for (let i of numero) {
+  if (repete == 2 || repete == 1) {
+    repete = 0
+  } else if (repete >= 3) {
+    return "não é possível gerar um número de telefone com esses valores"
+    repete = 0
+  }
+  for (let i2 = 0; i2 < numero.length; i2 += 1) {
+    if (i == numero[i2]) {
+      repete += 1;
+    }
+  }
+}
+  if (numero.length > 11) {
+    return "Array com tamanho incorreto."
+  }
+for (let i in numero) {
+  if (numero[i] < 0 || numero[i] > 9) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  if (i == 0) {
+    resultado = '('
+    resultado += numero[i]
+  }
+  if (i == 1) {
+    resultado += numero[i]
+    resultado += ')'
+  }
+  if (i == 2) {
+    resultado += ' '
+    resultado += numero[i]
+    for (let i = 3; i < 6; i += 1) {
+      resultado += numero[i]
+    }
+  }
+  if (i == 6) {
+    resultado += numero[i]
+    resultado += '-'
+    for (let i = 7; i < 11; i += 1) {
+      resultado += numero[i]
+    }
+  }
+} 
+return resultado
 }
 
 // Desafio 12
